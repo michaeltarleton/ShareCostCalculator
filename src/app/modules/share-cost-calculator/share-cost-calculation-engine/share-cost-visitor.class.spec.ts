f3d21d338b43e4d7ed69395f74082304f86f3367
Michael Tarleton <michael.tarleton@me.com>
2019-01-19 08:03:09 -0600

    master: Added remaining tests for ShareCostVisitor

--- src/app/modules/share-cost-calculator/share-cost-calculation-engine/share-cost-visitor.class.spec.ts
+++ src/app/modules/share-cost-calculator/share-cost-calculation-engine/share-cost-visitor.class.spec.ts
@@ -1,7 +1,7 @@
 import { async, TestBed } from '@angular/core/testing'
 import { CostDataService } from '@app/core/services/cost-data/cost-data.service'
 import { ShareCostVisitor } from './share-cost-visitor.class'
-import { LIFOShareCost } from './share-cost.class'
+import { FIFOShareCost, HighestCosthareCost, LIFOShareCost, LowestCostShareCost } from './share-cost.class'
 
 describe('ShareCostVisitor', () => {
   beforeEach(async(() => {
@@ -10,14 +10,62 @@ describe('ShareCostVisitor', () => {
     })
   }))
 
-  describe('ShareCostVisitor - LIFO', () => {
+  describe('LIFO', () => {
     it('should return 0', done => {
-      const lifoShareCost = new LIFOShareCost(0, 0, new Date())
+      const shareCost = new LIFOShareCost(0, 0, new Date())
       const costDataService = TestBed.get(CostDataService) as CostDataService
       costDataService.get().subscribe(shareRecords => {
         const sut = new ShareCostVisitor(shareRecords)
 
-        const result = sut.visit(lifoShareCost)
+        const result = sut.visit(shareCost)
+
+        expect(result).toBe(0)
+
+        done()
+      })
+    })
+  })
+
+  describe('FIFO', () => {
+    it('should return 0', done => {
+      const shareCost = new FIFOShareCost(0, 0, new Date())
+      const costDataService = TestBed.get(CostDataService) as CostDataService
+      costDataService.get().subscribe(shareRecords => {
+        const sut = new ShareCostVisitor(shareRecords)
+
+        const result = sut.visit(shareCost)
+
+        expect(result).toBe(0)
+
+        done()
+      })
+    })
+  })
+
+  describe('HighestCost', () => {
+    it('should return 0', done => {
+      const shareCost = new HighestCosthareCost(0, 0, new Date())
+      const costDataService = TestBed.get(CostDataService) as CostDataService
+      costDataService.get().subscribe(shareRecords => {
+        const sut = new ShareCostVisitor(shareRecords)
+
+        const result = sut.visit(shareCost)
+
+        expect(result).toBe(0)
+
+        done()
+      })
+    })
+  })
+
+  describe('LowestCost', () => {
+    it('should return 0', done => {
+      const shareCost = new LowestCostShareCost(0, 0, new Date())
+      const costDataService = TestBed.get(CostDataService) as CostDataService
+      costDataService.get().subscribe(shareRecords => {
+        const sut = new ShareCostVisitor(shareRecords)
+
+        const result = sut.visit(shareCost)
 
         expect(result).toBe(0)
 
